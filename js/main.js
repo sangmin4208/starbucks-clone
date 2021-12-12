0317dedb7c4c903683dd7a95e233b1a78565bbce
Lee Sang-Min <sangmin4208@gmail.com>
2021-12-12 23:11:25 +0900

    feat: 스크롤 애니메이션

--- js/main.js
+++ js/main.js
@@ -108,3 +108,14 @@ function floatingObject(selector,delay,size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 15);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+})
